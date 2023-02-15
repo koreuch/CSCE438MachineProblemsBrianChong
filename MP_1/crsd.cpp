e0a0618b011d1117cb378465ad6faf249bc1cd3b
438 <koreuch>
2023-02-14 18:56:11 -0700

    Checking that commits work on remote repository

--- MP_1/crsd.cpp
+++ MP_1/crsd.cpp
@@ -1,5 +1,20 @@
-#include <glog/logging.h>
+// #include <glog/logging.h>
+
 // TODO: Implement Chat Server.
+#include <glog/logging.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "interface.h"
+
+//including all the header files that are also in the client side code
 
 int main(int argc, char *argv[]){
     google::InitGoogleLogging(argv[0]);
